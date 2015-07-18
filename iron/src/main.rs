cb1b5b5d2eb2c04ae7a20b35f1ddc1ffcd839416
pfried <philipsfried@gmail.com>
2015-07-18 13:13:41 -0400

    added Router with url path parameters

--- iron/src/main.rs
+++ iron/src/main.rs
@@ -8,17 +8,32 @@ use iron::status;
 use staticfile::Static;
 use std::path::Path;
 use mount::Mount;
+use router::Router;
+use router::Params;
 
-fn handle_request(request: &mut Request) -> IronResult<Response> {
+fn hello_world(request: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "Hello World!")))
 }
 
+fn handle_get_with_path_variables(req: &mut Request) -> IronResult<Response> {
+    let params: &Params = req.extensions.get::<Router>().unwrap();
+    params.find("thing")
+        .and_then(|thing_param| params.find("adjective").map(|adj| (thing_param, adj)))
+        .map(|(thing, adjective)| format!("{} is like so totally {}!", thing, adjective))
+        .map(|response_string| Ok(Response::with((status::Ok, response_string))))
+        .unwrap()
+}
+
 fn main() {
-    let mut static_file_handler = Static::new(Path::new("static"));
+    let static_file_handler = Static::new(Path::new("static"));
+
+    let mut router = Router::new();
+    router.get("/:thing/:adjective", handle_get_with_path_variables);
 
     let mut mount = Mount::new();
-    mount.mount("/", handle_request)
-        .mount("/static", static_file_handler);
+    mount.mount("/hello", hello_world)
+        .mount("/static", static_file_handler)
+        .mount("/routes", router);
 
     Iron::new(mount)
         .http("localhost:3000")
