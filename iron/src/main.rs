f7bc09c53923d0d7490868e2b563e20d1086c825
pfried <philipsfried@gmail.com>
2015-07-18 00:16:48 -0400

    added static file handler and mount

--- iron/src/main.rs
+++ iron/src/main.rs
@@ -1,17 +1,26 @@
 extern crate iron;
+extern crate router;
+extern crate staticfile;
+extern crate mount;
 
-use iron::prelude::*;
+use iron::prelude::{Request, Response, IronResult, Iron};
 use iron::status;
-use iron::middleware::Handler;
-
-struct MyHandler;
+use staticfile::Static;
+use std::path::Path;
+use mount::Mount;
 
 fn handle_request(request: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "Hello World!")))
 }
 
 fn main() {
-    Iron::new(handle_request)
+    let mut static_file_handler = Static::new(Path::new("static"));
+
+    let mut mount = Mount::new();
+    mount.mount("/", handle_request)
+        .mount("/static", static_file_handler);
+
+    Iron::new(mount)
         .http("localhost:3000")
         .unwrap();
 }
